7f6405f10ae29c074da225e79c6b20785a546d16
ScooterCC <codycattan13@gmail.com>
2022-07-25 12:16:26 -0500

    CIS255

--- src/PayRoll.java
+++ src/PayRoll.java
@@ -1,56 +1,45 @@
 public class PayRoll {
 
+    // Employee ID
     private int[] employeeId = {5658845, 4520125, 7895122, 8477541, 8451277, 1302850, 7580489};
     private final int NUMBER_OF_EMPLOYEES = employeeId.length;
-    //Number of hours worked by each employee
-    private double[] hours;
-    //Employee's hourly pay rate
+    // Employee hours worked
+    private int[] hours;
+    // Employee pay rate
     private double[] payRate;
-    //Employee's gross wages.
+    // Employee gross wages
     private double[] wages;
 
-    //returns id of index
+    // Calculate employee wages
     public void calculateWages() {
         for (int employeeIndex = 0; employeeIndex < NUMBER_OF_EMPLOYEES; employeeIndex++) {
             wages[employeeIndex] = hours[employeeIndex] * payRate[employeeIndex];
         }
     }
 
-    public double getEmployeeWagesById(int employeeIdGiven) {
-        for (int employeeIndex = 0; employeeIndex < NUMBER_OF_EMPLOYEES; employeeIndex++) {
-            if (employeeId[employeeIndex] == employeeIdGiven) {
-                return wages[employeeIndex];
-            }
-        }
-        return -1;
-    }
-
+    // ID accessor
     public int[] getAllEmployeeIds() {
         return employeeId;
     }
-
-    public double[] getAllEmployeeHours() {
+    // Hours accessor
+    public int[] getAllEmployeeHours() {
         return hours;
     }
-
-    public double[] getAllEmployeePayRates() {
-        return payRate;
-    }
-
+    // Wages accessor
     public double[] getAllEmployeeWages() {
         return wages;
     }
-
+    // Hours mutator
     public void setEmployeeHours(int employeeHoursGiven, int employeeIndexGiven) {
         hours[employeeIndexGiven]=employeeHoursGiven;
     }
-
+    // Pay rate mutator
     public void setEmployeePayRate(double employeePayRateGiven, int employeeIndexGiven) {
-        hours[employeeIndexGiven]=employeePayRateGiven;
+        payRate[employeeIndexGiven]=employeePayRateGiven;
     }
-
+    // Constructor
     public PayRoll() {
-        hours = new double[NUMBER_OF_EMPLOYEES];
+        hours = new int[NUMBER_OF_EMPLOYEES];
         payRate = new double[NUMBER_OF_EMPLOYEES];
         wages = new double[NUMBER_OF_EMPLOYEES];
     }
--- src/PayRollProgram.java
+++ src/PayRollProgram.java
@@ -1,3 +1,4 @@
+// Scanner utility
 import java.util.Scanner;
 public class PayRollProgram {
     public static void assignEmployeeHoursAndPayrates(PayRoll payRollObject) {
@@ -5,10 +6,11 @@ public class PayRollProgram {
         int employeeHours;
         double employeePayRate;
 
+        // Ask user for data
         for (int employeeIndex = 0; employeeIndex < payRollObject.getAllEmployeeIds().length; employeeIndex++) {
             System.out.println("Please enter the hours for employee " + payRollObject.getAllEmployeeIds()[employeeIndex]);
             employeeHours = keyboard.nextInt();
-            while(employeeHours<0) {
+            while(employeeHours<0) { // Ask for a positive amount of hours worked
                 System.out.println("Please enter POSITIVE hours for employee " +
                         payRollObject.getAllEmployeeIds()[employeeIndex]);
                 employeeHours = keyboard.nextInt();
@@ -16,20 +18,21 @@ public class PayRollProgram {
             payRollObject.setEmployeeHours(employeeHours, employeeIndex);
             System.out.println("What's the pay rate for the employee " + payRollObject.getAllEmployeeIds()[employeeIndex]);
             employeePayRate = keyboard.nextDouble();
-            while(employeePayRate<6.00){
-                System.out.println("What's the pay rate for the employee "+
-                        payRollObject.getAllEmployeeIds()[employeeIndex] + "(ABOVE $6 please)");
+            while(employeePayRate<6.00){ // Ask for rate of $6 or greater
+                System.out.println("Enter the pay rate for the employee "+
+                        payRollObject.getAllEmployeeIds()[employeeIndex] + " (Pay rate must be greater than $6)");
                 employeePayRate = keyboard.nextDouble();
             }
             payRollObject.setEmployeePayRate(employeePayRate, employeeIndex);
         }
     }
-
+    // Display data
     public static void displayEmployeeWages(PayRoll payRollObject){
         for (int employeeIndex = 0; employeeIndex < payRollObject.getAllEmployeeIds().length; employeeIndex++) {
             System.out.println("The employee with ID number " + payRollObject.getAllEmployeeIds()[employeeIndex]+
                     " worked for " + payRollObject.getAllEmployeeHours()[employeeIndex] +
-                    " hours and earned $" + payRollObject.getAllEmployeeWages()[employeeIndex]);
+                    " hours and earned " +
+                    String.format("$%,.2f", payRollObject.getAllEmployeeWages()[employeeIndex]));
         }
     }
 
